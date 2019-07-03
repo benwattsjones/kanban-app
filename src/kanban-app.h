28f9973aed217f7103d61f92ae80d8c09f966d3e
Ben Watts-Jones <benwattsjones@gmail.com>
2019-07-03 13:34:48 +0100

    Initial setup: added copyright and license info
    Added COPYING file and brief boiler plate at top of file
    licensing the project under the terms of GPL-3.0-or-later.
    
    This license was taken from: <https://www.gnu.org/licenses/gpl-3.0.txt>
    
    Copyright info added to boiler plate as well.
    
    FUTURE NOTES:
     - Will need to extend copyright years if work done in other years.
     - Will need to change project name in boilerplate from 'kanban-app'
       to a better name as appropriate.
     - Boilerplate not needed in Makefile and other basic setup files.
     - May eventually have to add extra licenses to specific parts of
       project (e.g. documentation).

--- src/kanban-app.h
+++ src/kanban-app.h
@@ -1,3 +1,17 @@
+/* src/kanban-app.h
+ *
+ * Copyright (C) 2019 Ben Watts-Jones
+ *
+ * This file is part of kanban-app.
+ *
+ * kanban-app is released under the terms of the GNU General Public License,
+ * either version 3 of the license, or (at your option) any later version.
+ * You should have received a copy of the GNU General Public License along 
+ * with kanban-app. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 #ifndef KANBAN_APP_H
 #define KANBAN_APP_H
 
