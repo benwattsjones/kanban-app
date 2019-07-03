8dd7e977f7e92a2fe9bcbfe13d154863d582864d
Ben Watts-Jones <benwattsjones@gmail.com>
2019-07-03 15:42:50 +0100

    Initial setup .h: added G_{BEGIN,END}_DECLS guards
    G_BEGIN_DECLS and G_END_DECLS macros around declarations
    in header files not strictly required here, but enable
    safe use with c++ files in future.

--- src/kanban-app-win.h
+++ src/kanban-app-win.h
@@ -18,14 +18,16 @@
 #include <gtk/gtk.h>
 #include "kanban-app.h"
 
+G_BEGIN_DECLS
 
 #define KANBAN_APP_WINDOW_TYPE (kanban_app_window_get_type ())
-G_DECLARE_FINAL_TYPE (KanbanAppWindow, kanban_app_window, KANBAN, APP_WINDOW, GtkApplicationWindow)
 
+G_DECLARE_FINAL_TYPE (KanbanAppWindow, kanban_app_window, KANBAN, APP_WINDOW, GtkApplicationWindow)
 
 KanbanAppWindow        *kanban_app_window_new          (KanbanApp *app);
 void                    kanban_app_window_open         (KanbanAppWindow *win,
                                                         GFile           *file);
-
+G_END_DECLS
 
 #endif /* KANBAN_APP_WIN_H */
+
--- src/kanban-app.h
+++ src/kanban-app.h
@@ -17,13 +17,15 @@
 
 #include <gtk/gtk.h>
 
+G_BEGIN_DECLS
 
 #define KANBAN_APP_TYPE (kanban_app_get_type ())
 
 G_DECLARE_FINAL_TYPE (KanbanApp, kanban_app, KANBAN, APP, GtkApplication)
 
-
 KanbanApp     *kanban_app_new         (void);
 
+G_END_DECLS
 
 #endif /* KANBAN_APP_H */
+
