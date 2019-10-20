0f8fe2c2fbfbbb4df0170e7bdb1a718c5d267d35
Ben Watts-Jones <benwattsjones@gmail.com>
2019-10-20 19:07:15 +0100

    column-observable-interface-tests: add tests
    Test added to check KanbanColumnViewModel can be successfully
    cast to KanbanColumnObservable interface, can in turn be cast
    to GListModel interface, ensuring interfaces can be used. Test
    added to check _get_heading() function of interface works.
    
    All tests pass inc. valgrind and clang.

--- tests/kanban-column-observable-interface-tests.cpp
+++ tests/kanban-column-observable-interface-tests.cpp
@@ -23,6 +23,7 @@ extern "C"
 #include "gtest/gtest.h"
 
 #define COLUMN_ID 1
+#define COLUMN_HEADING "col heading"
 
 extern "C"
 {
@@ -36,7 +37,7 @@ protected:
 
   void SetUp() override
   {
-    viewmodel = kanban_column_viewmodel_new (COLUMN_ID, "col heading");
+    viewmodel = kanban_column_viewmodel_new (COLUMN_ID, COLUMN_HEADING);
   }
 
   void TearDown() override
@@ -49,6 +50,31 @@ protected:
 TEST_F (KanbanColumnObservableInterfaceTests,
         New_ValidKanbanColumnViewModelPassed_InterfaceAvailable)
 {
-  SUCCEED ();
+  KanbanColumnObservable *column_iface;
+  GListModel *list_iface;
+  column_iface = KANBAN_COLUMN_OBSERVABLE (viewmodel);
+  list_iface = G_LIST_MODEL (column_iface);
+  EXPECT_TRUE (KANBAN_IS_COLUMN_OBSERVABLE (column_iface));
+  EXPECT_TRUE (G_IS_LIST_MODEL (list_iface));
+}
+
+TEST_F (KanbanColumnObservableInterfaceTests,
+        GetHeading_ValidKanbanColumnViewModelPassed_PointerToHeadingReturned)
+{
+  KanbanColumnObservable *column_iface;
+  GtkTextIter start, end;
+  gchar *buffer_text;
+  GtkTextBuffer *heading;
+  column_iface = KANBAN_COLUMN_OBSERVABLE (viewmodel);
+
+  heading = kanban_column_observable_get_heading (column_iface);
+  gtk_text_buffer_get_bounds (heading, &start, &end);
+  buffer_text = gtk_text_buffer_get_text (heading, &start, &end, FALSE);
+
+  ASSERT_NE (heading, nullptr);
+  ASSERT_TRUE (GTK_IS_TEXT_BUFFER (heading));
+  EXPECT_STREQ (buffer_text, COLUMN_HEADING);
+
+  g_free (buffer_text);
 }
 
