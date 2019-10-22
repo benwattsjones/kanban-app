f64b8d898a184f9aa83a2b3ce96e45e56f5f77c5
Ben Watts-Jones <benwattsjones@gmail.com>
2019-10-22 22:21:02 +0100

    kanban-column-view: add test for removing card
    Test added to check number of card widgets in GtkListBox
    container decrements when a card is moved from the corresponding
    GListModel viewmodel column to a different column.
    
    All tests pass, inc. valgrind, clang and release.

--- tests/kanban-column-view-tests.cpp
+++ tests/kanban-column-view-tests.cpp
@@ -92,3 +92,30 @@ TEST_F (KanbanColumnViewTests,
   g_list_free (cards_added_after);
 }
 
+TEST_F (KanbanColumnViewTests,
+        MoveColum_CardRemovedFromViewModelList_CardRemovedFromViewList)
+{
+  KanbanColumnViewModel *viewmodel2;
+  GSequenceIter *card_iter;
+  GList *cards_added_before, *cards_added_after, *l;
+  int num_columns_before = 0, num_columns_after = 0;
+  card_iter = kanban_column_viewmodel_new_card (viewmodel, &card_data);
+  cards_added_before = gtk_container_get_children (GTK_CONTAINER (column_contents));
+  for (l = cards_added_before; l != NULL; l = l->next)
+    ++num_columns_before;
+
+  viewmodel2 = kanban_column_viewmodel_new (card_data.column_id+1, "Column Heading");
+  kanban_column_viewmodel_move_card (viewmodel, viewmodel2,
+                                     card_iter, card_data.priority);
+  cards_added_after = gtk_container_get_children (GTK_CONTAINER (column_contents));
+  for (l = cards_added_after; l != NULL; l = l->next)
+    ++num_columns_after;
+
+  EXPECT_EQ (num_columns_after, num_columns_before - 1);
+
+  g_list_free (cards_added_before);
+  g_list_free (cards_added_after);
+  kanban_column_viewmodel_destroy (viewmodel2);
+}
+
+
